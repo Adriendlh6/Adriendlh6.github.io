d369b900e23eb99c849ca9c7a6b7602f6a93af6d
Adriendlh6 <161251091+Adriendlh6@users.noreply.github.com>
2026-04-24 15:09:43 +0200

    Rename 'Gestion des données d’entreprise' to 'Entreprise'

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,7 @@ const ROUTES = {
   mercuriale: { title: 'Mercuriale', file: 'pages/mercuriale.html' },
   fournisseurs: { title: 'Fournisseurs', file: 'pages/fournisseurs.html' },
   recettes: { title: 'Recettes', file: 'pages/recettes.html' },
-  monentreprise: { title: 'Gestion des données d’entreprise', file: 'pages/mon-entreprise.html' },
+  monentreprise: { title: 'Entreprise', file: 'pages/mon-entreprise.html' },
   parametres: { title: 'Paramètres', file: 'pages/parametres.html' },
 };
 
